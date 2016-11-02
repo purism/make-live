a15f5d8430453ab8abf3916e97cbcc7744a901b2
Matthias Klumpp <matthias@tenstral.net>
2016-11-02 21:55:32 +0100

    No more debootstrap symlinking
    This was a hack which we don't need anymore.
    (PureOS has its own script for green now)

--- helper/make_chroot.sh
+++ helper/make_chroot.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-ln -sf /usr/share/debootstrap/scripts/testing /usr/share/debootstrap/scripts/green
-
 debootstrap \
 	--keyring=/usr/share/keyrings/pureos-archive-keyring.gpg \
 	green \
 	lbchroot \
 	http://repo.puri.sm/pureos
-
-chroot lbchroot 'ln -s /usr/share/debootstrap/scripts/testing /usr/share/debootstrap/scripts/green'
